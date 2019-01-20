0a787df458b7706cd6f9fef7959876814430761d
eric-klimowich <eklimowich@hotmail.com>
2019-01-19 20:29:48 -0500

    Begin conditional render of login options

--- src/LogIn.js
+++ src/LogIn.js
@@ -2,17 +2,43 @@ import React from 'react'
 import NewUser from './NewUser'
 import ReturningUser from './ReturningUser'
 
+  function handleClick(event) {
+    console.log(event.target.value)
+  }
+
+  function renderLoginComponent(props) {
+    return (
+      <div>
+        <NewUser
+          submitNewUser={props.submitNewUser}
+        />
+        <ReturningUser
+          users={props.users}
+          chooseReturningUser={props.chooseReturningUser}
+        />
+      </div>
+    )
+  }
+
 const LogIn = props => {
 
   return (
     <div>
-      <NewUser
-        submitNewUser={props.submitNewUser}
-      />
-      <ReturningUser
-        users={props.users}
-        chooseReturningUser={props.chooseReturningUser}
-      />
+      <button
+        name="signup"
+        value="signup"
+        onClick={handleClick}
+      >
+        Sign Up
+      </button>
+      <button
+        name="login"
+        value="login"
+        onClick={handleClick}
+      >
+        Log In
+      </button>
+      <div>{renderLoginComponent(props)}</div>
     </div>
   )
 
