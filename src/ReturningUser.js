27a8fd222cf1ce82ca43be480dae6fbd1d151fde
eric-klimowich <eklimowich@hotmail.com>
2019-01-20 12:14:53 -0500

    Added input form for username and password to Returning user with handle change and submit

--- src/ReturningUser.js
+++ src/ReturningUser.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function fullName(firstName, lastName) {
-  const fullName = `${firstName} ${lastName}`
-  return fullName
-}
+class ReturningUser extends Component {
 
-const ReturningUser = props => {
-  // console.log('In ReturningUser: ', props)
-  return (
-    <form>
-      <select onChange={props.chooseReturningUser}>
-        <option>Users:</option>
-        {props.users.map(user => <option key={user.id} value={user.id} >{fullName(user.first_name, user.last_name)}</option>)}
-      </select>
-    </form>
-  )
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = (event) => {
+    // console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event, loginInput) => {
+    event.preventDefault()
+    console.log(loginInput)
+  }
+
+  render() {
+    // console.log('In ReturningUser: ', props)
+    return (
+      <form onSubmit={(event) => this.handleSubmit(event, this.state)}>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          placeholder="Enter username..."
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          value={this.state.password}
+          placeholder="Enter password..."
+          onChange={this.handleChange}
+        />
+        <input
+          type="submit"
+        />
+      </form>
+    )
+  }
+
 }
 
 export default ReturningUser
+
+// <select onChange={this.props.chooseReturningUser}>
+// <option>Users:</option>
+// {this.props.users.map(user => <option key={user.id} value={user.id} >{fullName(user.first_name, user.last_name)}</option>)}
+// </select>
